fd65c75a1e324bfbe97cc3419d764121c85c39d2
NazmulHasanNahid <nazmulhasan3394@gmail.com>
2022-02-28 23:03:50 +0600

    mobile section

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ const phone = () =>{
 const showPhone  = (phones)=>{
 const phoneContainer = document.getElementById('phone-container')
 phoneContainer.innerHTML = '' ;
+document.getElementById('phone-container').value = '' ;
  phones.forEach(phone =>{
   //  console.log(phone);
     const div = document.createElement('div')
@@ -30,15 +31,26 @@ phoneContainer.innerHTML = '' ;
  })
 }
 
-// const moreInfo = (id) =>{
-//  fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
-//   .then(res => res.json())
-//   .then(data => showInfo(data.data))
-// }
+const moreInfo = (id) =>{
 
-// const showInfo = (information) =>{
-//   const infoContainer = document.getElementById('info-container')
-//   for(const info of information){
-//       console.log(info);
-//   }
-// } 
+ fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  .then(res => res.json())
+  .then(data => showInfo(data.data))
+}
+
+const showInfo = (information) =>{
+  const infoContainer = document.getElementById('info-container')
+  const div = document.createElement('div')
+  div.classList.add('col')
+  div.innerHTML= ` 
+  <div class="card">
+  <img width="400px"  src="${information.image}" class="card-img-top " alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Brand: ${information.brand}</h5>
+    <p class="card-text"> Model: ${information.phone_name}</p>
+    <p class="card-text"> Relese Date: ${information.releaseDate}</p>
+  </div>
+</div>
+  `
+  infoContainer.appendChild(div)
+} 
