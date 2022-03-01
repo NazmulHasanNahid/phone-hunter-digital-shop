935e9d4b27a1f3c8fcb4b8e70253832ed0f20edb
NazmulHasanNahid <nazmulhasan3394@gmail.com>
2022-03-01 12:35:39 +0600

    others info

--- js/app.js
+++ js/app.js
@@ -19,11 +19,10 @@ phoneContainer.innerHTML = '' ;
     div.classList.add('col')
     div.innerHTML = ` 
     <div class="card p-3 text-center shadow-lg ">
-    <img width="400px"  src="${phone.image}" class="card-img-top " alt="...">
+    <img  src="${phone.image}" class="card-img-top  w-50 mx-auto" alt="...">
     <div class="card-body border-0  ">
       <h5 class="card-title">Brand: ${phone.brand}</h5>
       <p class="card-text"> Model: ${phone.phone_name}</p>
-      <p class="card-text"> Relese Date: ${phone.releaseDate}</p>
       <button onClick="moreInfo('${phone.slug}')" class="btn btn-info text-white">Details</button>
     </div>
   </div>
@@ -41,22 +40,31 @@ const moreInfo = (id) =>{
 }
 
 const showInfo = (information) =>{
- // console.log(information.mainFeatures.sensors);
+ console.log(information);
   const infoContainer = document.getElementById('info-container')
   infoContainer.innerHTML = '' ;
   const div = document.createElement('div')
   div.classList.add('col')
   div.innerHTML= ` 
   <div class="card p-3 shadow-lg">
-  <img width="400px"  src="${information.image}" class="card-img-top img-fluid" alt="...">
+  <img   src="${information.image}" class="card-img-top img-fluid w-50 mx-auto" alt="...">
   <div class="card-body">
-    <h5 class="card-title"> <span class="fw-bold">Brand</span> : ${information.brand}</h5>
+    <p class="card-title"> <span class="fw-bold">Brand</span> : ${information.brand}</p>
     <p class="card-text"> <span class="fw-bold">Model</span> : ${information.name}</p>
     <p class="card-text"><span class="fw-bold"> ChipSet</span> : ${information.mainFeatures.chipSet}</p>
     <p class="card-text"> <span class="fw-bold"> Disply Size</span> : ${information.mainFeatures.displaySize}</p>
     <p class="card-text"> <span class="fw-bold">Memory</span>  : ${information.mainFeatures.memory}</p>
     <p class="card-text"> <span class="fw-bold">Sensor</span>  : ${information.mainFeatures.sensors}</p>
     <p class="card-text"> <span class="fw-bold">Relese Date</span>  : ${information.releaseDate}</p>
+    <p class="text-center text-info">Others Information <br> _________</p>
+    <p class="card-text"> <span class="fw-bold">NFC</span>  : ${information.others.NFC}</p>
+    <p class="card-text"> <span class="fw-bold">Bluetooth</span>  : ${information.others.Bluetooth}</p>
+    <p class="card-text"> <span class="fw-bold">Radio</span>  : ${information.others.Radio}</p>
+    <p class="card-text"> <span class="fw-bold">Usb</span>  : ${information.others.USB}</p>
+    <p class="card-text"> <span class="fw-bold">GPS</span>  : ${information.others.GPS}</p>
+    <p class="card-text"> <span class="fw-bold">WALN</span>  : ${information.others.WLAN}</p>
+    
+
   </div>
 </div>
   `
